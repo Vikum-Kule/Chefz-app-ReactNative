93da8080a4633d0b9b1c3b8cd3f6bb3a4b43999d
Vikum-Kule <vikumkulatunga@gmail.com>
2021-10-06 08:31:50 +0530

    initial commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text> Hello Screen..</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
